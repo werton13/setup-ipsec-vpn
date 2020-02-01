c96a6e538c3a1109068f4587091a2e8e2e7f0d7e
werton13 <werton13@gmail.com>
2020-02-01 20:51:33 +0300

    update install.sh

--- azure/install.sh
+++ azure/install.sh
@@ -5,6 +5,19 @@ export VPN_USER=$2
 export VPN_PASSWORD=$3
 export VPN_RMGWIP=$4
 
+export VPN_LEFTSUBNET=$5
+export VPN_LEFTID=$6
+
+export VPN_RIGHTSUBNET=$7
+
+export VPN_CONNAME=$8
+export VPN_IPSEC_KEYEXCHANGE=$9
+export VPN_IPSEC_IKEV2=$10
+export VPN_IPSEC_IKEPHAZE1=$11
+export VPN_IPSEC_IKEPHAZE2ESP=$12
+export VPN_IPSEC_IKEPHAZE2ALG=$13
+export VPN_IPSEC_PFS=$14
+
 # Wait 60 seconds for apt/dpkg lock
 sleep 60
 
